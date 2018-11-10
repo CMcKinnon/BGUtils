42033e5777fc33f85e95ff7cb2f103c751e85d10
Corey McKinnon <cmckinnon@gmail.com>
2018-11-10 13:37:32 -0700

    Strip out bootstrap, jquery, extra pages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,35 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BGUtils.Models;
 using Microsoft.AspNetCore.Mvc;
-using BGUtils.Models;
+using System.Diagnostics;
 
 namespace BGUtils.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
