1a1023cdccc801dd6a7b3cd135d713f3b0442f5c
Corey McKinnon <cmckinnon@gmail.com>
2018-11-10 13:27:49 -0700

    initial create

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BGUtils.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
