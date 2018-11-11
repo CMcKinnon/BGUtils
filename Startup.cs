d9940b2d913df3517a38b5dfc3b40ee415932664
Corey McKinnon <cmckinnon@gmail.com>
2018-11-11 10:22:08 -0700

    Add reference to BGG client

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace BGUtils
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddBGGClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
